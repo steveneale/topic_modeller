436f41a8c875f720750868705fee253fc82724b8
Steve Neale <steveneale3000@gmail.com>
2019-07-08 23:37:03 +0100

    Adjust formatting
    Removed an unwanted blank line from src/io/utils.py (to keep the code
    in line with PEP8)

--- src/io/utils.py
+++ src/io/utils.py
@@ -37,7 +37,6 @@ def load_pickled_object(file_path):
 
 
 def create_directory(directory_path):
-
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
     return directory_path
